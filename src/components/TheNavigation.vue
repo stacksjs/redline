a1021d1f732a82a972c161884501f3cb457b1060
Adelino Ngomacha <adelinob335@gmail.com>
2025-08-14 12:51:42 +0200

    chore: wip

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -154,39 +154,35 @@ onUnmounted(() => {
             Contact Us
           </button>
 
-          <!-- Dark Mode Toggle - Connect to global state -->
-          <div class="group relative w-11 inline-flex shrink-0 rounded-full bg-gray-100 p-0.5 transition-colors duration-200 ease-in-out">
-            <span
-              class="ring-border relative size-5 rounded-full bg-white ring-1 transition-transform duration-200 ease-in-out"
-              :class="{ 'translate-x-5': isDark }"
+          <!-- Dark Mode Button - Icon only -->
+          <button
+            class="rounded-full p-2 ring-offset-white transition-all duration-200 ease-in-out focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500 dark:ring-offset-black"
+            :class="isDark ? 'text-blue-400 hover:bg-gray-800' : 'bg-gray-200 text-gray-600 hover:bg-gray-300'"
+            :aria-label="isDark ? 'Switch to light mode' : 'Switch to dark mode'"
+            @click="() => toggleDark()"
+          >
+            <!-- Sun icon for light mode (shows when in light mode) -->
+            <svg
+              v-show="!isDark"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="size-5"
             >
-              <span
-                class="absolute inset-0 size-full flex items-center justify-center transition-opacity duration-200 ease-in"
-                :class="{ 'opacity-0': isDark, 'opacity-100': !isDark }"
-                aria-hidden="true"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-3 text-yellow-400">
-                  <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
-                </svg>
-              </span>
-              <span
-                class="absolute inset-0 size-full flex items-center justify-center transition-opacity duration-200 ease-in"
-                :class="{ 'opacity-100': isDark, 'opacity-0': !isDark }"
-                aria-hidden="true"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-3 text-blue-400">
-                  <path fill-rule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clip-rule="evenodd" />
-                </svg>
-              </span>
-            </span>
-            <input
-              :checked="isDark"
-              type="checkbox"
-              class="absolute inset-0 appearance-none focus:outline-hidden"
-              aria-label="Toggle dark mode"
-              @click="() => toggleDark()"
+              <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+            </svg>
+
+            <!-- Moon icon for dark mode (shows when in dark mode) -->
+            <svg
+              v-show="isDark"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="size-5"
             >
-          </div>
+              <path fill-rule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clip-rule="evenodd" />
+            </svg>
+          </button>
         </div>
 
         <!-- Mobile Menu Button -->
